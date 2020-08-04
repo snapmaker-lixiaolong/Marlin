f53c724e53be950ef138f5855f82c0ee092a26de
lixiaolong <786503628@qq.com>
2020-08-04 12:53:14 +0800

    <Feature>:<use snapmaker original parameters>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -736,68 +736,79 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
-
-/**
- * Default Max Feed Rate (mm/s)
- * Override with M203
- *                                      X, Y, Z, E0 [, E1[, E2...]]
- */
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {(200.0*16/8),(200.0*16/8),(200.0*16/8),(200.0*16/3.1415926/11.0)}  // default steps per unit for Ultimaker
+  
+  
+  /**
+   * Default Max Feed Rate (mm/s)
+   * Override with M203
+   *                                      X, Y, Z, E0 [, E1[, E2...]]
+   */
 #define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
-
-//#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
+  
+  //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
   #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
 #endif
-
-/**
- * Default Max Acceleration (change/s) change = mm/s
- * (Maximum start speed for accelerated moves)
- * Override with M201
- *                                      X, Y, Z, E0 [, E1[, E2...]]
- */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
-
-//#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
+  
+  /**
+   * Default Max Acceleration (change/s) change = mm/s
+   * (Maximum start speed for accelerated moves)
+   * Override with M201
+   *                                      X, Y, Z, E0 [, E1[, E2...]]
+   */
+  //#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      {1000,1000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+  
+  
+  //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
   #define MAX_ACCEL_EDIT_VALUES       { 6000, 6000, 200, 20000 } // ...or, set your own edit limits
 #endif
-
-/**
- * Default Acceleration (change/s) change = mm/s
- * Override with M204
- *
- *   M204 P    Acceleration
- *   M204 R    Retract Acceleration
- *   M204 T    Travel Acceleration
- */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
-
-/**
- * Default Jerk limits (mm/s)
- * Override with M205 X Y Z E
- *
- * "Jerk" specifies the minimum speed change that requires acceleration.
- * When changing speed and direction, if the difference is less than the
- * value set here, it may happen instantaneously.
- */
-//#define CLASSIC_JERK
+  
+  /**
+   * Default Acceleration (change/s) change = mm/s
+   * Override with M204
+   *
+   *   M204 P    Acceleration
+   *   M204 R    Retract Acceleration
+   *   M204 T    Travel Acceleration
+   */
+  //#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
+  //#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
+  //#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration in mm/s^2 for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration in mm/s^2 for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration in mm/s^2 for travel (non printing) moves
+  
+  /**
+   * Default Jerk limits (mm/s)
+   * Override with M205 X Y Z E
+   *
+   * "Jerk" specifies the minimum speed change that requires acceleration.
+   * When changing speed and direction, if the difference is less than the
+   * value set here, it may happen instantaneously.
+   */
+  //#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
-  #define DEFAULT_XJERK 10.0
-  #define DEFAULT_YJERK 10.0
-  #define DEFAULT_ZJERK  0.3
-
-  //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
-
-  //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
+  #define DEFAULT_XJERK 20.0
+  #define DEFAULT_YJERK 20.0
+  #define DEFAULT_ZJERK  0.4
+  #define DEFAULT_EJERK  5.0  // May be used by Linear Advance
+    // The speed change that does not require acceleration (i.e. the software might assume it can be done instantaneously)
+  
+  
+    //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
+  
+    //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
   #if ENABLED(LIMITED_JERK_EDITING)
     #define MAX_JERK_EDIT_VALUES { 20, 20, 0.6, 10 } // ...or, set your own edit limits
   #endif
 #endif
-
+  
 #define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+
 
 /**
  * Junction Deviation Factor
